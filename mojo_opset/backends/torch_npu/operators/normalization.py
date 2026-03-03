d1b4af7d215d75a44f2c23f11ee742aba44814c5
mc-zhang <zhangbolun6@huawei.com>
2026-03-03 17:58:54 +0800

    bug fix

--- mojo_opset/backends/torch_npu/operators/normalization.py
+++ mojo_opset/backends/torch_npu/operators/normalization.py
@@ -9,3 +9,22 @@ from mojo_opset.core import MojoResidualAddLayerNorm
 
 
 
+class TorchNpuLayerNorm(MojoLayerNorm):
+    supported_platforms_list = ["npu"]
+
+
+class TorchNpuRMSNorm(MojoRMSNorm):
+    supported_platforms_list = ["npu"]
+    
+    def forward(self, hidden_state: torch.Tensor) -> torch.Tensor:
+
+        return torch_npu.npu_rms_norm(hidden_state, self.weight, eps=self.variance_epsilon)
+
+class TorchNpuResidualAddRMSNorm(MojoResidualAddRMSNorm):
+    supported_platforms_list = ["npu"] 
+
+    def forward(self, hidden_state: torch.Tensor, residual: torch.Tensor) -> torch.Tensor:
+        hidden_state, _, residual = torch_npu.npu_add_rms_norm(hidden_state, residual, self.weight, eps=self.variance_epsilon)
+        if self.norm_pos != "pre":
+            residual = hidden_state
+        return hidden_state, residual
