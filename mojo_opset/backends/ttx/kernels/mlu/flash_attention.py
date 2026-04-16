b557eac5945ea303aac876de87e5aee884e53f36
Xing Hongbo <xinghongbo@cambricon.com>
2026-04-16 17:55:28 +0800

    tile_k == block_size时简化代码

--- mojo_opset/backends/ttx/kernels/mlu/flash_attention.py
+++ mojo_opset/backends/ttx/kernels/mlu/flash_attention.py
@@ -18,7 +18,7 @@ def paged_attention_decode_kernel(
     context_lens_ptr,
     softmax_scale,
     block_size: tl.constexpr,
-    blocks_per_seq: tl.constexpr,
+    blocks_per_seq,
     bs,
     q_heads: tl.constexpr,
     kv_heads: tl.constexpr,
@@ -35,14 +35,12 @@ def paged_attention_decode_kernel(
     stride_o_batch,
     stride_o_head,
     stride_bt_batch,
+    stride_bt_nblk,
     gqa_interleave,
-    ):
+):
     q_div_k : tl.constexpr = q_heads // kv_heads
     tile_q  : tl.constexpr = q_div_k
-    tile_k  : tl.constexpr = 1024  # tile_k >= block_size
-    seg_size: tl.constexpr = tile_k if tile_k < block_size else block_size
-    block_num_per_core: tl.constexpr = (tile_k + block_size - 1) // block_size  # block_num_per_core > 0
-    block_num_pad_up  : tl.constexpr = (blocks_per_seq + block_num_per_core - 1 ) // block_num_per_core * block_num_per_core # block_num_pad_up is 0 ???
+    tile_k  : tl.constexpr = block_size  # tile_k >= block_size
 
     total_tasks = bs * kv_heads
     core_nums = tl.num_programs(0)
@@ -62,27 +60,22 @@ def paged_attention_decode_kernel(
         # seq_len
         context_len = tl.load(context_lens_ptr + batch_id)
 
-        # load block_table
-        block_table_offset = stride_bt_batch * batch_id
-        block_table_seg = tl.arange(0, block_num_pad_up)
-        block_table_ptrs = block_table_ptr + block_table_offset + block_table_seg
-        block_ids = tl.load(block_table_ptrs, mask=block_table_seg < blocks_per_seq, other=0)
-
         # output
         o = tl.zeros((tile_q, head_size_vo), dtype=tl.float32)
         rmax, cmax = tl.full((tile_q,), -float('inf'), dtype=tl.float32), tl.zeros((tile_q,), dtype=tl.float32)
         rsum, csum = tl.zeros((tile_q,), dtype=tl.float32), tl.zeros((tile_q,), dtype=tl.float32)
 
-        for k_begin in range(0, context_len, tile_k): # tile_k = m * block_size
+        for k_begin in range(0, context_len, tile_k): # tile_k = block_size
+            # load block_table
             block_begin = k_begin // block_size
-            block_ids_per_core = block_ids[block_begin: block_begin + block_num_per_core]
+            block_id = tl.load(block_table_ptr + stride_bt_batch * batch_id + block_begin * stride_bt_nblk)
             seq_k = tile_k if context_len > k_begin + tile_k else context_len - k_begin
 
             # load kcache
-            kcache_offset = block_ids_per_core * stride_k_nblk + kv_head_id * stride_k_head
-            kcache_seg = tl.arange(0, seg_size * head_size_qk)
-            kcache_ptrs = kcache_ptr + kcache_offset[:, None] + kcache_seg[None, :]
-            kcache_data = tl.load(kcache_ptrs).reshape(tile_k, head_size_qk)  # seg_size == tile_k || seg_size * block_num_per_core == tile_k
+            kcache_offset = block_id * stride_k_nblk + kv_head_id * stride_k_head
+            kcache_seg = tl.arange(0, tile_k * head_size_qk)
+            kcache_ptrs = kcache_ptr + kcache_offset + kcache_seg
+            kcache_data = tl.load(kcache_ptrs).reshape(tile_k, head_size_qk)
 
             qk = tl.dot(q_data, tl.trans(kcache_data), out_dtype=tl.float32) * softmax_scale
             cut_off_mask = tl.arange(0, tile_k) < seq_k
@@ -100,19 +93,19 @@ def paged_attention_decode_kernel(
             rmax = cmax
 
             # load vcache
-            vcache_offset = block_ids_per_core * stride_v_nblk + kv_head_id * stride_v_head
-            vcache_seg = tl.arange(0, seg_size * head_size_vo)
-            vcache_ptrs = vcache_ptr + vcache_offset[:, None] + vcache_seg[None, :]
+            vcache_offset = block_id * stride_v_nblk + kv_head_id * stride_v_head
+            vcache_seg = tl.arange(0, tile_k * head_size_vo)
+            vcache_ptrs = vcache_ptr + vcache_offset + vcache_seg
             vcache_data = tl.load(vcache_ptrs).reshape(tile_k, head_size_vo)
 
             qkv = tl.dot(p_cast, vcache_data, out_dtype=tl.float32)
 
             o = o * alpha[:, None] + qkv
         # end for
+        lse = rmax
         if context_len > 0:
             o = o / rsum[:, None]
-
-        lse = rmax + tl.log(rsum)
+            lse += tl.log(rsum)
 
         # store output
         o_offset = batch_id * stride_o_batch + q_head_ids * stride_o_head
@@ -174,6 +167,7 @@ def paged_attention_decode_impl(
         stride_o_batch,
         stride_o_head,
         stride_bt_batch,
+        stride_bt_nblk,
         gqa_interleave,
     )
 
